98fb8b052c5e9b5d2a4aff567741b2dc5875fcd6
Gustavo Siñovsky <g.sinovsky@gmail.com>
2016-05-18 21:36:49 -0430

    Pep8 fixes

--- app/models/model.py
+++ app/models/model.py
@@ -2,12 +2,10 @@
 
 # Se importan las librerias necesarias.
 import os
-import datetime
-from flask                import Flask
-from flask.ext.migrate    import Migrate, MigrateCommand
+from flask import Flask
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script     import Manager
-from sqlalchemy           import DateTime
+from flask.ext.script import Manager
 
 # Conexion con la base de datos.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -27,30 +25,27 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Definicion de la Base de Datos.
 
 class classUser (db.Model):
     '''Clase que define el modelo Usuario'''
 
     __tablename__ = 'user'
-    userId   = db.Column(db.Integer, primary_key=True, index=True)
-    email    = db.Column(db.String(30), unique=True)
+    userId = db.Column(db.Integer, primary_key=True, index=True)
+    email = db.Column(db.String(30), unique=True)
     fullname = db.Column(db.String(50))
     password = db.Column(db.String(200))
-    
 
     def __init__(self, email, fullname, password):
         '''Constructor del modelo usuario'''
-        self.email    = email
+        self.email = email
         self.fullname = fullname
         self.password = password
-        
 
     def __repr__(self):
         '''Representacion en string del modelo Usuario'''
-        return '<fullname %r, email %r, username %r >' % (self.userId, self.email, self.fullname)
-
-
+        return \
+            '<fullname %r, email %r, username %r >' % (self.userId, self.email,
+                                                       self.fullname)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
