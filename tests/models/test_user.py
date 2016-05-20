f2a66b647c45a0da1f1c445f5d5bf2f7b074a660
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-05-20 03:40:04 -0430

    core repeatable test of user model

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -1,10 +1,22 @@
-
 import unittest
+
+from app.models.db import db
+from app.models.user import User
+
 
 # Placeholder Test case to make a model test file
 class TestUserModel(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def setUp(self):
+        db.create_all()
+        self.User = User()
+
+    def tearDown(self):
+        db.drop_all()
+
+    def test_createCommonUser(self):
+        userCreated = self.User.createUser("manuelalejandropm@gmail.com", "Manuel Pacheco", "somepassword")
+        self.assertEqual(userCreated['status'], 'success');
+
 
 if __name__ == '__main__':
     unittest.main()
