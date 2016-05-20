9b743f6bf57e01acf4eb3f5e882d4204b6055314
Gabriel Russo <globerusso@gmail.com>
2016-05-20 04:26:05 -0430

    configuracion de sesion

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,4 +1,12 @@
 from app import app
+from flask import session
+from datetime import timedelta
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=45)
+    session.modified = True
 
 @app.route('/')
 def root():
