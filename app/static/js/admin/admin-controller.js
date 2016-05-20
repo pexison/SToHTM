aa80732dcdcc6c20effbb9c3d1f3e7c911ddf389
Gabriel Russo <globerusso@gmail.com>
2016-05-19 21:48:56 -0430

    controladores de administrador actualizados

--- app/static/js/admin/admin-controller.js
+++ app/static/js/admin/admin-controller.js
@@ -20,10 +20,12 @@ stohtModule.controller('adminController',
 
 
 
-stohtModule.controller('userListController',
+stohtModule.controller('editUserController',
    ['$scope', '$location', '$route', 'flash', 'adminService',
     function ($scope, $location, $route, flash, adminService) {
         $scope.msg = "";
+        $scope.formTitle = "Editar Usuario";
+        $scope.saveButton = "Actualizar";
         $scope.logout = function() {
             $location.path('/');
           //validate login then call a WS for redirect
@@ -38,4 +40,71 @@ stohtModule.controller('userListController',
            */
       };
 
+        $scope.save = function() {
+            $location.path('/userList');
+          //validate login then call a WS for redirect
+            /*
+            loginService.validar({user: $scope.user, pass: $scope.pass}).then(function (object) {
+            if(object.data['error'] != undefined){
+              $scope.error = object.data['error'];
+            }else{
+              $location.path('/login');
+            }
+          });
+           */
+      };
+
+    }]);
+
+stohtModule.controller('userListController',
+   ['$scope', '$location', '$route', 'flash', 'adminService',
+    function ($scope, $location, $route, flash, adminService) {
+        $scope.msg = "";
+        $scope.formTitle = "Nuevo Usuario";
+        $scope.saveButton = "Crear";
+        $scope.users = [{fullName: "Gabriel Russo", email:"globerusso@gmail.com", rol:"Admin" }]
+        for (i=0;i<30;i++){
+           $scope.users[i]= {id: i, fullName: "Gabriel Russo", email:"globerusso@gmail.com", rol:"Admin" }
+        }
+        $scope.viewUser = function(id) {
+            $location.path('/editUser');
+          //validate login then call a WS for redirect
+            /*
+            loginService.validar({user: $scope.user, pass: $scope.pass}).then(function (object) {
+            if(object.data['error'] != undefined){
+              $scope.error = object.data['error'];
+            }else{
+              $location.path('/login');
+            }
+          });
+           */
+      };
+
+        $scope.save = function() {
+            $location.path('/userList');
+          //validate login then call a WS for redirect
+            /*
+            loginService.validar({user: $scope.user, pass: $scope.pass}).then(function (object) {
+            if(object.data['error'] != undefined){
+              $scope.error = object.data['error'];
+            }else{
+              $location.path('/login');
+            }
+          });
+           */
+      };
+
+        $scope.logout = function() {
+            $location.path('/');
+          //validate login then call a WS for redirect
+            /*
+            loginService.validar({user: $scope.user, pass: $scope.pass}).then(function (object) {
+            if(object.data['error'] != undefined){
+              $scope.error = object.data['error'];
+            }else{
+              $location.path('/login');
+            }
+          });
+           */
+      };
     }]);
