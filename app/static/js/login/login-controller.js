bbc95a80218e697292c846395d493b37949b3b1c
Gabriel Russo <globerusso@gmail.com>
2016-05-21 16:15:24 -0430

    control de acceso y cierre de sesion

--- app/static/js/login/login-controller.js
+++ app/static/js/login/login-controller.js
@@ -1,10 +1,19 @@
 stohtModule.controller('loginController',
    ['$scope', '$location', '$route', 'flash', 'loginService',
     function ($scope, $location, $route, flash, loginService) {
+        loginService.check({'securityLvl': 0}).then(function (object) {
+                if (object.data['redirect'] == undefined) {
+                    $scope.actorName = object.data['actorName'];
+                    $scope.actorRol = object.data['actorRol'];
+                } else {
+                    $location.path(object.data['redirect']);
+                }
+            });
         $scope.msg = "";
 
+        //Iniciar sesion
         $scope.authenticate = function() {
-            
+
             loginService.authenticate({email: $scope.email, password: $scope.password}).then(function (object) {
             if(object.data['error'] != undefined){
                 $scope.msg = object.data['error'];
@@ -19,3 +28,6 @@ stohtModule.controller('loginController',
       };
 
     }]);
+
+
+
