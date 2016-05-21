e5b95035440c5e899107050c52d1f24a8dd1cb23
Gabriel Russo <globerusso@gmail.com>
2016-05-21 16:17:53 -0430

    nueva app para sesiones y permisos

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ db = SQLAlchemy(app)
 # Route and config registration
 from app.routes import index
 from app.api.user_api import user
+from app.api.session_api import auth
 
 app.config.from_object('config')
 app.register_blueprint(user)
+app.register_blueprint(auth)
