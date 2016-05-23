39c585f0f1123e3296175179a4203550a7de08ac
Gabriel Russo <globerusso@gmail.com>
2016-05-22 21:29:23 -0430

    controlador de registro

---
+++ app/static/js/signup/signup-controller.js
@@ -0,0 +1,47 @@
+//controlador de registro
+stohtModule.controller('signupController',
+   ['$scope', '$location', '$route', 'flash', 'signupService','loginService',
+    function ($scope, $location, $route, flash, signupService, loginService) {
+        loginService.check({'securityLvl': 0}).then(function (object) {
+                if (object.data['redirect'] == undefined) {
+                    $scope.actorName = object.data['actorName'];
+                    $scope.actorRol = object.data['actorRol'];
+                } else {
+                    $location.path(object.data['redirect']);
+                }
+            });
+        $scope.msg = "";
+
+        //Registrar
+        $scope.save = function() {
+            if ($scope.password == $scope.passwordr) {
+                signupService.signup({
+                    email: $scope.email,
+                    password: $scope.password,
+                    rol: 2,
+                    fullName: $scope.fullName
+                }).then(function (object) {
+                    if (object.data['error'] != undefined) {
+                        $scope.msg = object.data['error'];
+                    } else {
+                        if (object.data['status'] != 'failure') {
+                            $location.path('/');
+                        }else{
+                            $scope.msg = object.data['reason'];
+                        }
+                    }
+                });
+            }else{
+                $scope.msg="Las contraseñas no coinciden";
+            }
+
+      };
+
+        //volver al login
+        $scope.login = function() {
+            $location.path('/');
+        };
+
+    }]);
+
+
