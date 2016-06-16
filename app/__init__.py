87595de9e525d7e0471bc1f7ea4b2c577e227e75
Gustavo Siñovsky <g.sinovsky@gmail.com>
2016-06-16 13:03:07 -0430

    includes services api

--- app/__init__.py
+++ app/__init__.py
@@ -11,9 +11,11 @@ from app.api.user_api import user
 from app.api.session_api import auth
 from app.api.profile_api import profile
 from app.api.categories_api import category
+from app.api.services_api import service
 
 app.config.from_object('config')
 app.register_blueprint(user)
 app.register_blueprint(auth)
 app.register_blueprint(profile)
 app.register_blueprint(category)
+app.register_blueprint(service)
---
+++ app/api/services_api.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, json, request
+
+from app.models.service import Service
+
+service = Blueprint('service', __name__,)
+
+
+@service.route('/services/create', methods=['POST'])
+def create_service():
+    name = request.args.get('name')
+    category = request.args.get('category') or None
+    user = request.args.get('user') or None
+    if request.args.get('name') is None or len(request.args.get('name')) == 0:
+        res = {'error': 'You must provide a valid service name.'}
+    if request.args.get('category') is None or len(request.args.get('category')) == 0:
+        res = {'error': 'You must provide a valid category.'}
+    if request.args.get('user') is None or len(request.args.get('user')) == 0:
+        res = {'error': 'You must provide a valid user.'}
+    else:
+        ServiceInstance = Service()
+        result = ServiceInstance.createService(
+            name, category, user)
+        res = result
+
+    return json.dumps(res)
+
+
+@service.route('/services', methods=['GET'])
+def get_services():
+    ServiceInstance = Service()
+    services = ServiceInstance.getServices()
+    rescat = []
+    for service in services:
+        rescat.append({'id': service.serviceId,
+                       'name': service.name,
+                       'category': service.category,
+                       'user': service.user})
+
+    return json.dumps(rescat)
+
+
+@service.route('/service', methods=['GET'])
+def get_service():
+    name = request.args.get('name')
+    if request.args.get('name') is None or len(request.args.get('name')) == 0:
+        res = {'error': 'You must provide a valid service name.'}
+    else:
+        ServiceInstance = Service()
+        service = ServiceInstance.getServiceByName(name)
+        res = {'id': service.serviceId,
+               'name': service.name,
+               'category': service.category,
+               'user': service.user}
+
+    return json.dumps(res)
+
+
+@service.route('/services/delete', methods=['POST'])
+def delete_service():
+    id = int(request.args.get('id'))
+    if request.args.get('id') is None or len(request.args.get('id')) == 0:
+        res = {'error': 'You must provide a valid service id.'}
+    else:
+        ServiceInstance = Service()
+        result = ServiceInstance.deleteService(id)
+        res = result
+
+    return json.dumps(res)
+
+
+@service.route('/services/update', methods=['POST'])
+def update_service():
+    id = int(request.args.get('id'))
+    name = request.args.get('name') or None
+    category = request.args.get('category') or None
+    user = request.args.get('user') or None
+    if request.args.get('id') is None or len(request.args.get('id')) == 0:
+        res = {'error': 'You must provide a valid category id.'}
+    else:
+        ServiceInstance = Service()
+        result = ServiceInstance.updateService(
+            id, name, category, user)
+        res = result
+
+    return json.dumps(res)
