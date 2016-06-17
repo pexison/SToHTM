adda1dbe15d04093e2649c7488729d5be34ca98b
Gabriel Russo <globerusso@gmail.com>
2016-06-17 00:50:41 -0400

    nueva ruta y directiva

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,5 @@
 // Creación del módulo de la aplicación
-var stohtModule = angular.module('eventos', ['ngRoute', 'ngAnimate', 'flash']);
+var stohtModule = angular.module('eventos', ['angularTreeview','ngRoute', 'ngAnimate', 'flash']);
 stohtModule.config(function ($routeProvider) {
     $routeProvider.when('/', {
                 controller: 'loginController',
@@ -25,6 +25,9 @@ stohtModule.config(function ($routeProvider) {
             }).when('/categories', {
                 controller: 'categoriesController',
                 templateUrl: 'views/app/categories.html'
+            }).when('/services', {
+                controller: 'servicesController',
+                templateUrl: 'views/app/services.html'
             })
 });
 
@@ -32,3 +35,37 @@ stohtModule.controller('stohtController_',  ['$scope', '$http', '$location',
 function($scope) {
     $scope.title = "login";
 }]);
+
+
+/*
+    angular.treeview.js
+*/
+(function(l){
+	l.module("angularTreeview",[]).directive("treeModel",function($compile){
+		return{
+			restrict:"A",link:function(a,g,c){
+				var e=c.treeModel,
+				 h=c.nodeLabel || "label",
+				 d=c.nodeChildren || "children",
+				 k ='<ul><li data-ng-repeat="node in '+e+'" ng-init= "node.collapsed = true"><i class="collapsed" data-ng-show="node.'+d+'.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="expanded" data-ng-show="node.'+d+'.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="normal" data-ng-hide="node.'+	d+'.length"></i><span data-ng-class="node.selected" data-ng-click="selectNodeLabel(node, $event)">{{node.'+h+'}}</span><div data-ng-hide="node.collapsed" data-tree-model="node.'+d+'" data-node-id='+(c.nodeId||"id")+" data-node-label="+h+" data-node-children="+d+"></div></li></ul>";
+			e && e.length && (c.angularTreeview?(a.$watch(e,function(m,b){
+					g.empty().html($compile(k)(a))
+				},!1),a.selectNodeHead=a.selectNodeHead||function(a,b){
+					b.stopPropagation&&b.stopPropagation();
+					b.preventDefault&&b.preventDefault();
+					b.cancelBubble=!0;
+					b.returnValue=!1;
+					a.collapsed=!a.collapsed
+				},a.selectNodeLabel=a.selectNodeLabel||function(c,b){
+					b.stopPropagation&&b.stopPropagation();
+					b.preventDefault&&b.preventDefault();
+					b.cancelBubble=!0;
+					b.returnValue=!1;
+					a.currentNode&&a.currentNode.selected&&(a.currentNode.selected=void 0);
+					c.selected="selected";
+					a.currentNode=c}
+					):g.html($compile(k)(a)))}
+				}
+			}
+			)}
+)(angular);
